51d3a56fd75d678b071eeeba7c3e686b4a67fdc3
Matheus Marcondes <90806719+MathMarcondes@users.noreply.github.com>
2023-06-28 13:42:49 -0300

    Alteração no account

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MySuperBank
     {
         static void Main(string[] args)
         {
-            var account = new BankAccount("Kendra", 10000);
+            var account = new BankAccount("Atila Uno", 10000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with US${account.Balance}.");
 
             account.MakeWithdrawal(500, DateTime.Now, "Rent Paymant");
